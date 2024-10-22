38c92ac6a2633077cdb438b1b23d16940e47668a
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2024-10-22 11:08:40 +0200

    ft: add spectrogram_plot method to visualizate an audio object as a spectrogram

---
+++ pysoundlocalization/visualization/spectrogram_plot.py
@@ -0,0 +1,19 @@
+from core.Audio import Audio
+import matplotlib.pyplot as plt
+
+
+def spectrogram_plot(audio: Audio):
+    sample_rate = audio.sample_rate
+    audio_signal = audio.get_audio_signal()
+
+    plt.figure(figsize=(10, 6))
+
+    plt.specgram(audio_signal, NFFT=1024, Fs=sample_rate, noverlap=512, cmap="inferno")
+
+    plt.title("Spectrogram of Audio Signal")
+    plt.ylabel("Frequency [Hz]")
+    plt.xlabel("Time [s]")
+
+    plt.colorbar(label="Intensity [dB]")
+
+    plt.show()
