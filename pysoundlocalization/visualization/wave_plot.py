a24a3ca81872a3761d55a4107264fa168e7c5cb2
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2024-10-22 23:31:15 +0200

    ft: added wave_plot() that can plot a waveform by providing an Audio object

---
+++ pysoundlocalization/visualization/wave_plot.py
@@ -0,0 +1,25 @@
+from core.Audio import Audio
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def wave_plot(audio: Audio):
+    """
+    Plot the waveform of an audio signal.
+
+    :param audio: Audio object containing the audio signal to plot.
+    """
+    plt.figure(figsize=(10, 6))
+
+    timeline = np.linspace(
+        0,
+        len(audio.get_audio_signal()) / audio.get_sample_rate(),
+        len(audio.get_audio_signal()),
+    )
+
+    plt.plot(timeline, audio.get_audio_signal())
+    plt.title("Waveform")
+    plt.ylabel("Amplitude")
+    plt.xlabel("Time (seconds)")
+
+    plt.show()
