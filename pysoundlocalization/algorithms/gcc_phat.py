4d89d79c20702fbfa995a98b0d0e7ccf2d1d275e
972C8 <me@nineseven.one>
2024-11-04 15:26:53 +0100

    correctly reference code via apache license

--- pysoundlocalization/algorithms/gcc_phat.py
+++ pysoundlocalization/algorithms/gcc_phat.py
@@ -1,20 +1,12 @@
 """
- Estimate time delay using GCC-PHAT 
- Copyright (c) 2017 Yihui Xiong
+This script demonstrates the GCC-PHAT algorithm for estimating time delays between signals.
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+Original code by Yihui Xiong (2017), licensed under the Apache License, Version 2.0.
+Source: https://github.com/xiongyihui/tdoa/blob/master/gcc_phat.py
+License: https://www.apache.org/licenses/LICENSE-2.0
 
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+Adapted as part of a project for sound localization.
 """
-# TODO: how to reference the creator of the code?
 
 import numpy as np
 
