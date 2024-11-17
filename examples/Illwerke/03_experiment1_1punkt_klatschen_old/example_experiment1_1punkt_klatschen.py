b79834b3d30df6b8e40cb099bf4bd7767330b041
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2024-11-17 10:41:43 +0100

    ref: fix

--- examples/Illwerke/03_experiment1_1punkt_klatschen_old/example_experiment1_1punkt_klatschen.py
+++ examples/Illwerke/03_experiment1_1punkt_klatschen_old/example_experiment1_1punkt_klatschen.py
@@ -5,6 +5,8 @@ from pysoundlocalization.preprocessing.SampleRateConverter import SampleRateConv
 from pysoundlocalization.preprocessing.FrequencyFilterChain import FrequencyFilterChain
 from pysoundlocalization.preprocessing.LowCutFilter import LowCutFilter
 from pysoundlocalization.visualization.spectrogram_plot import spectrogram_plot
+from pysoundlocalization.visualization.multilaterate_plot import multilaterate_plot
+from pysoundlocalization.visualization.wave_plot import wave_plot
 
 # Create simulation and add an environment with 4 microphones
 simulation = Simulation.create()
@@ -33,29 +35,52 @@ mic4.set_audio(Audio(filepath=audio4_filepath))
 # Ensure that all audio files are of same sample rate
 SampleRateConverter.convert_all_to_lowest_sample_rate(environment1)
 
-spectrogram_plot(mic1.get_audio())
+spectrogram_plot(
+    mic1.get_audio().get_audio_signal_by_index(index=0),
+    mic1.get_audio().get_sample_rate(),
+)
 
 frequency_filter_chain = FrequencyFilterChain()
-frequency_filter_chain.add_filter(LowCutFilter(cutoff_frequency=15000, order=5))
+frequency_filter_chain.add_filter(LowCutFilter(cutoff_frequency=5000, order=5))
 frequency_filter_chain.apply(mic1.get_audio())
 frequency_filter_chain.apply(mic2.get_audio())
 frequency_filter_chain.apply(mic3.get_audio())
 frequency_filter_chain.apply(mic4.get_audio())
 
-spectrogram_plot(mic1.get_audio())
+spectrogram_plot(
+    mic1.get_audio().get_audio_signal_by_index(index=0),
+    mic1.get_audio().get_sample_rate(),
+)
+
+wave_plot(
+    mic1.get_audio().get_audio_signal_by_index(index=0),
+    mic1.get_audio().get_sample_rate(),
+)
+
+algorithm_choice = "gcc_phat"
+
+dict = environment1.multilaterate(
+    algorithm=algorithm_choice, number_of_sound_sources=1, threshold=0.15
+)
+mic1.get_audio().play()
+
+for i, object in enumerate(dict):
+    print(dict[object])
+
+multilaterate_plot(environment1, dict)
 
 # Compute all TDoA and DoA for all mic pairs
-tdoa_pairs = environment1.compute_all_tdoa(
-    print_intermediate_results=True,
-)
-print(f"TDoA for all mic pairs: {tdoa_pairs}")
+# tdoa_pairs = environment1.compute_all_tdoa(
+#     print_intermediate_results=True,
+# )
+# print(f"TDoA for all mic pairs: {tdoa_pairs}")
 
-doa_pairs = environment1.compute_all_doa(tdoa_pairs, print_intermediate_results=True)
-print(f"DoA for all mic pairs: {doa_pairs}")
+# doa_pairs = environment1.compute_all_doa(tdoa_pairs, print_intermediate_results=True)
+# print(f"DoA for all mic pairs: {doa_pairs}")
 
-# Approximate and visualize the sound source position
-x, y = environment1.multilaterate_sound_source(tdoa_pairs)
-print(f"Approximated source position: x={x}, y={y}")
+# # Approximate and visualize the sound source position
+# x, y = environment1.multilaterate_sound_source(tdoa_pairs)
+# print(f"Approximated source position: x={x}, y={y}")
 
-environment1.add_sound_source_position(x, y)
-environment1.visualize()
+# environment1.add_sound_source_position(x, y)
+# environment1.visualize()
