6ed824561dadceb745ee55b887d0487b9bc19720
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2024-12-03 17:38:57 +0100

    ft: added wave plot and spectrogram methods for passing the whole environment

--- pysoundlocalization/visualization/spectrogram_plot.py
+++ pysoundlocalization/visualization/spectrogram_plot.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pysoundlocalization.core.Environment import Environment
 
 
 def spectrogram_plot(audio_signal: np.ndarray, sample_rate: int) -> None:
@@ -21,4 +22,37 @@ def spectrogram_plot(audio_signal: np.ndarray, sample_rate: int) -> None:
 
     plt.colorbar(label="Intensity [dB]")
 
+    plt.show()
+
+
+def spectrogram_plot_environment(environment: Environment) -> None:
+    """
+    Plot spectrograms of all audio signals in the given environment.
+
+    Args:
+        environment: The environment object containing microphones with audio signals.
+    """
+    mics = environment.get_mics()
+
+    num_mics = len(mics)
+    plt.figure(figsize=(10, 2 * num_mics))
+
+    for i, mic in enumerate(mics):
+        audio_signal = mic.get_audio().get_audio_signal_unchunked()
+
+        plt.subplot(num_mics, 1, i + 1)
+        plt.specgram(
+            audio_signal,
+            NFFT=1024,
+            Fs=mic.get_audio().get_sample_rate(),
+            noverlap=512,
+            cmap="inferno",
+        )
+        plt.ylabel("Frequency [Hz]")
+        plt.xlabel("Time [s]")
+
+        plt.colorbar(label="Intensity [dB]")
+
+    plt.gcf().canvas.manager.set_window_title("Environment all Spectrograms")
+    plt.tight_layout()
     plt.show()
--- pysoundlocalization/visualization/wave_plot.py
+++ pysoundlocalization/visualization/wave_plot.py
@@ -1,6 +1,7 @@
 from pysoundlocalization.core.Audio import Audio
 import matplotlib.pyplot as plt
 import numpy as np
+from pysoundlocalization.core.Environment import Environment
 
 
 def wave_plot(
@@ -27,4 +28,35 @@ def wave_plot(
     plt.ylabel("Amplitude")
     plt.xlabel("Time (seconds)")
 
+    plt.show()
+
+
+def wave_plot_environment(environment: Environment) -> None:
+    """
+    Plot the waveforms of all audio signals in the given environment.
+
+    Args:
+        environment: The environment object containing microphones with audio signals.
+    """
+    mics = environment.get_mics()
+
+    num_mics = len(mics)
+    plt.figure(figsize=(10, 2 * num_mics))
+
+    for i, mic in enumerate(mics):
+        audio_signal = mic.get_audio().get_audio_signal_unchunked()
+
+        timeline = np.linspace(
+            0,
+            len(audio_signal) / mic.get_audio().get_sample_rate(),
+            len(audio_signal),
+        )
+
+        plt.subplot(num_mics, 1, i + 1)
+        plt.plot(timeline, audio_signal)
+        plt.ylabel("Amplitude")
+        plt.xlabel("Time (seconds)")
+
+    plt.gcf().canvas.manager.set_window_title("Environment all Waveplots")
+    plt.tight_layout()
     plt.show()
